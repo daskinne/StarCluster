9d1a6497a8003e8df975e45fd65d234494e6927e
David Skinner <david.skinnera@gmail.com>
2013-07-07 00:10:01 +0000

    added tag for assigned cluster

--- starcluster/plugins/tags.py
+++ starcluster/plugins/tags.py
@@ -7,6 +7,7 @@ class TagsPlugin(ClusterSetup):
     """
     Plugin that configures environment variables for a cluster
     """
+    sg_prefix = 'SecurityGroup:@sc-'
     def __init__(self, tag_list=None,tag_file=None):
         tag_s = []
         if not tag_list is None:
@@ -24,6 +25,8 @@ class TagsPlugin(ClusterSetup):
         self.tags = dict([t.split('=') for t in tag_s])
 
     def run(self, nodes, master, user, user_shell, volumes):
+        if not "cluster_assignment" in self.tags: 
+            self.tags["cluster_assignment"] = master.parent_cluster.replace(self.sg_prefix,'');
         for tag in self.tags:
             val = self.tags.get(tag)
             log.info("Applying tag - %s: %s" % (tag, val))
@@ -31,6 +34,8 @@ class TagsPlugin(ClusterSetup):
                 node.add_tag(tag, val)
 
     def on_add_node(self, node, nodes, master, user, user_shell, volumes):
+        if not "cluster_assignment" in self.tags:
+            self.tags["cluster_assignment"] = master.parent_cluster.replace(self.sg_prefix,'');
         for tag in self.tags:
             val = self.tags.get(tag)
             log.info("Applying tag - %s: %s" % (tag, val))
